482a699624755d0e5297e8326196ddabb1c1fd4b
linuxdaemon <linuxdaemon.irc@gmail.com>
2025-11-04 22:02:28 +0000

    fix(core): remove broken/inconsistent restart handler
    Restarting the process by `exec`-ing itself has led to various issues with the process hanging, getting into a weird state, etc. Let's just rely on the service manager to deal with restarts
    
    BREAKING CHANGE: Technically this removes a feature, but it wasn't utilized by the existing code anyways and only caused trouble

--- bncbot/main.py
+++ bncbot/main.py
@@ -4,47 +4,20 @@
 # SPDX-License-Identifier: MIT
 
 import asyncio
-import os
-import signal
-import sys
-import time
-from types import FrameType
-
-import aiofiles
+import logging
 
 from bncbot import bot
 from bncbot.conn import Conn
 
 
-async def async_main() -> bool:
-    async with aiofiles.open(".bncbot.pid", "w", encoding="utf8") as pid_file:
-        await pid_file.write(str(os.getpid()))
-
+async def async_main() -> None:
     conn = Conn(bot.HANDLERS)
 
-    original_sigint = signal.getsignal(signal.SIGINT)
-
-    def handle_sig(sig: int, frame: FrameType | None) -> None:
-        if sig == signal.SIGINT:
-            if conn:
-                asyncio.run_coroutine_threadsafe(conn.shutdown(), conn.loop)
-
-            signal.signal(signal.SIGINT, original_sigint)
-
-    signal.signal(signal.SIGINT, handle_sig)
-
-    return await conn.run()
+    await conn.run()
 
 
 def main() -> None:
-    # store the original working directory, for use when restarting
-    original_wd = os.path.realpath(".")
-    restart = asyncio.run(async_main())
-    if restart:
-        time.sleep(1)
-        os.chdir(original_wd)
-        args = sys.argv
-        for f in [sys.stdout, sys.stderr]:
-            f.flush()
-
-        os.execv(sys.executable, [sys.executable] + args)
+    try:
+        asyncio.run(async_main())
+    finally:
+        logging.shutdown()
