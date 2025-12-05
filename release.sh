39ad9bc96640e12c323512dba3155b288f3e6861
linuxdaemon <linuxdaemon.irc@gmail.com>
2025-12-05 01:55:04 +0000

    ci(release): correct tag formats

--- release.sh
+++ release.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 
 changelog_file="$(mktemp)"
-cz bump --changelog-to-stdout --git-output-to-stderr > "$changelog_file"
+cz bump --yes --changelog-to-stdout --git-output-to-stderr > "$changelog_file"
 
 git push origin main
 sleep 1
 git push --tags
 sleep 1
-gh release create "v$(cz version -p)" --verify-tag -t "Release v$(cz version -p)" -F "$changelog_file"
+gh release create "$(cz version -p)" --verify-tag -t "Release $(cz version -p)" -F "$changelog_file"
 rm "$changelog_file"
